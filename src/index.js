35c4a4925d117529c8d976c18f4b765c13ea5819
Jamie <alaryj@gmail.com>
2018-12-18 18:25:57 -0700

    file structure

--- src/index.js
+++ src/index.js
@@ -2,14 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import NoMatch from './pages/404';
-import App from './demo_page/index';
 import Home from './home';
-import BlogDetails from './blog/blog-details';
-import BlogList from './blog/blog-list';
-import BlogLeftSidebar from './blog/blog-leftsidebar';
-import BlogRightSidebar from './blog/blog-rightsidebar';
-import BlogLeftside from './blog/blog-leftside';
-import BlogRightside from './blog/blog-rightside';
 import SignIn from './pages/sign-in';
 import SignUp from './pages/sign-up';
 import ForgetPassword from './pages/forget-pwd';
@@ -28,14 +21,7 @@ class Root extends React.Component {
   	return(
   		<BrowserRouter basename={'/'} >
 		  	<Switch>
-			  <Route exact path={`${process.env.PUBLIC_URL}/`} component={App}/>
 			  <Route path={`${process.env.PUBLIC_URL}/home`} component={Home}/>
-			  <Route path={`${process.env.PUBLIC_URL}/blog-details`} component={BlogDetails}/>
-			  <Route path={`${process.env.PUBLIC_URL}/blog-list`} component={BlogList}/>
-			  <Route path={`${process.env.PUBLIC_URL}/blog-left-sidebar`} component={BlogLeftSidebar}/>
-			  <Route path={`${process.env.PUBLIC_URL}/blog-right-sidebar`} component={BlogRightSidebar}/>
-			  <Route path={`${process.env.PUBLIC_URL}/blog-leftside`} component={BlogLeftside}/>
-			  <Route path={`${process.env.PUBLIC_URL}/blog-rightside`} component={BlogRightside}/>
 			  <Route path={`${process.env.PUBLIC_URL}/sign-in`} component={SignIn}/>
 			  <Route path={`${process.env.PUBLIC_URL}/sign-up`} component={SignUp}/>
 			  <Route path={`${process.env.PUBLIC_URL}/forget-password`} component={ForgetPassword}/>
